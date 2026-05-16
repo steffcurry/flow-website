38e18dbad14f5c7525e198888c61f87d2990b353
steffcurry <stefcurry543@gmail.com>
2026-05-16 17:36:04 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -11,7 +11,7 @@ import Privacy from './pages/Privacy';
 import Terms from './pages/Terms';
 import Pay from './pages/Pay';
 import Form from './pages/Form';
-import ROI from '.pages/ROICalculator';
+import Roi from '.pages/ROICalculator.tsx';
 
 function App() {
   return (
